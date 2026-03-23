84a2c14053e2cafbb418287821578745555ce953
athGurnani <atharvgurnani@gmail.com>
2026-03-23 18:30:10 -0500

    Update TeleOpDrive example

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apexPathing/examples/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apexPathing/examples/TeleOpDrive.java
@@ -4,11 +4,16 @@ import static org.firstinspires.ftc.teamcode.apexPathing.Constants.dtConstants;
 import static org.firstinspires.ftc.teamcode.apexPathing.Constants.localizer;
 
 import com.apexpathing.drivetrain.MecanumDrive;
+import com.apexpathing.follower.Follower;
+import com.apexpathing.follower.PTPFollower;
 import com.apexpathing.util.math.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.apexPathing.Constants;
 
+/**
+ * @author Atharv Gurnani - 13085 Bionic Dutch
+ */
 public class TeleOpDrive extends OpMode {
     public MecanumDrive drive;
 
@@ -16,27 +21,22 @@ public class TeleOpDrive extends OpMode {
     public void init() {
         drive = new MecanumDrive(
                 hardwareMap,
-                localizer,
-                dtConstants,
-                "fl",
-                "fr",
-                "bl",
-                "br"
+                dtConstants
         );
+
         Constants.init();
     }
 
     @Override
     public void loop() {
         telemetry.update();
-        drive.update();
 
-        Pose currentPose = drive.getPose();
-        drive.setDrivePowers(new Pose(
+        Pose currentPose = localizer.getPose();
+        drive.botCentricDrive(
                 gamepad1.left_stick_x,
                 -gamepad1.left_stick_y,
                 gamepad1.right_stick_x
-        ));
+        );
 
         telemetry.addData("Bot X", currentPose.x());
         telemetry.addData("Bot Y", currentPose.y());
